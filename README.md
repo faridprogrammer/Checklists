6a2582e9658c8e4a22db7a7608d863ee4f93df60
Farid Bekran <faridbekran15@gmail.com>
2022-01-30 15:10:23 +0330

    Add gatsby

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
+> _My books are always tactical, bullet lists, this is what you need to do because I'm trying to appeal to people who are trying to change the world and they need **checklists**._
+> 
+> _Guy Kawasaki_
+> 
+
 # Checklists
 Number of useful checklists for personal use
+
+# Where is it? 
+
+To see the list of checklists (:D) go to the following address.
+
+
+# What is it? (Realy??!!)
+
+This repository contains some checklists for those who think the checklists are useful and using them. 
+The checklists available here can be gathered from elsewhere which in these cases the source of checklists is provided to the user.
+
+# Why?
+
+Certainly there are excelent websites and apps to find checklists. But I needed a simple alternative which can be easily managed.
+
+
+# Contribute
+
+You can add your checklist to this repository by adding a markdown file to the `website/content/blog` folder. 
+Your checklist should have the following conditions.
+
+1. It should be new (Not been mentioned in the repository before)
+2. If it is from another source you need to mention the source. 
+3. It cannot contain adult or any explicit content. 
+
+
+
---
+++ website/src/pages/index.js
@@ -0,0 +1,81 @@
+import * as React from "react"
+import { Link, graphql } from "gatsby"
+import kebabCase from "lodash/kebabCase"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const BlogIndex = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const posts = data.allMarkdownRemark.nodes
+
+  if (posts.length === 0) {
+    return (
+      <Layout location={location} title={siteTitle}>
+        <Seo title="All" />
+        <p>
+          No check lists found. Add markdown posts to "content/blog" (or the
+          directory you specified for the "gatsby-source-filesystem" plugin in
+          gatsby-config.js).
+        </p>
+      </Layout>
+    )
+  }
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Seo title="All" />
+      <ol style={{ listStyle: `none` }}>
+        {posts.map(post => {
+          const title = post.frontmatter.title || post.fields.slug
+          return (
+            <li key={post.fields.slug}>
+              <article
+                className="post-list-item"
+                itemScope
+                itemType="http://schema.org/Article"
+              >
+                <header>
+                  <h5>
+                    <Link to={post.fields.slug} itemProp="url">
+                      <span itemProp="headline">{title}</span>
+                    </Link>
+                  </h5>
+                  <small><b>{post.frontmatter.date}</b></small>
+                  {post.frontmatter.tags.map(tag => { return (<small class='label info'> <Link style={{"font-size":10, textDecoration:"none", color:"white"}} to={`/tags/${kebabCase(tag)}/`}>{tag}</Link> </small>) })}
+                </header>
+              </article>
+            </li>
+          )
+        })}
+      </ol>
+    </Layout>
+  )
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+          tags
+        }
+      }
+    }
+  }
+`
